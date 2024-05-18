d2af7305916a5afade7a56895e11d8764472b669
Robert Odhiambo <odhizrober254@gmail.com>
2024-05-18 11:13:29 +0300

    test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,11 +3,14 @@
 import unittest
 from parameterized import parameterized
 from typing import Dict, Tuple, Union
+from utils import (
+    access_nested_map,
+)
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ TestAccessNestedMap class definition """
-    @parametarized.expand([
+    @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
